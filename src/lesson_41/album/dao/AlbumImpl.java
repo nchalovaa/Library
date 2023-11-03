b836e30cc4e248f59077b9605f9990387cc5adf1
Chalova Anastasiia <nchalova93@gmail.com>
2023-11-03 10:20:44 +0100

    update

---
+++ src/lesson_41/album/dao/AlbumImpl.java
@@ -0,0 +1,48 @@
+package lesson_41.album.dao;
+
+import lesson_41.album.model.Photo;
+
+import java.time.LocalDate;
+
+public class AlbumImpl implements Album{
+    private  Photo[] photos;
+    private int size;
+
+    public AlbumImpl(int capacity){
+        this.photos = new Photo[capacity];
+    }
+    @Override
+    public boolean addPhoto(Photo photo) {
+        return false;
+    }
+
+    @Override
+    public boolean removePhoto(int photoId, int albumId) {
+        return false;
+    }
+
+    @Override
+    public boolean updatePhoto(int photoId, int albumId, String url) {
+        return false;
+    }
+
+    @Override
+    public Photo getPhotoFromAlbum(int photoId, int albumId) {
+        return null;
+    }
+
+    @Override
+    public Photo[] getAllPhotoFromAlbum(int albumId) {
+        return new Photo[0];
+    }
+
+    @Override
+    public Photo[] getPhotoBetweenDate(LocalDate dateFrom, LocalDate dateTo) {
+        return new Photo[0];
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
